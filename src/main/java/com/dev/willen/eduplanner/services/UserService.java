c5c5acb5edf4ed5472452d795c597b1f21befef9
willen01 <willen.sesantos@gmail.com>
2025-02-02 14:37:25 -0300

    chore: Implementa hash de senha para usuário

--- src/main/java/com/dev/willen/eduplanner/services/UserService.java
+++ src/main/java/com/dev/willen/eduplanner/services/UserService.java
@@ -4,15 +4,18 @@ import com.dev.willen.eduplanner.dto.CreateUserDto;
 import com.dev.willen.eduplanner.entities.User;
 import com.dev.willen.eduplanner.exceptions.DuplicatedUser;
 import com.dev.willen.eduplanner.repositories.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
     private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository repository) {
+    public UserService(UserRepository repository, PasswordEncoder passwordEncoder) {
         this.repository = repository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public void registerUser(CreateUserDto userDto) {
@@ -24,7 +27,7 @@ public class UserService {
         userEntity.setFirstname(userDto.firstname());
         userEntity.setLastname(userDto.lastname());
         userEntity.setEmail(userDto.email());
-        userEntity.setPassword(userDto.password());
+        userEntity.setPassword(passwordEncoder.encode(userDto.password()));
 
         repository.save(userEntity);
     }
