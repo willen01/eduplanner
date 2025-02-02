9812f3fabe9dddb8c6bfd32a3cd89c46e0e79e03
willen01 <willen.sesantos@gmail.com>
2025-02-02 13:56:53 -0300

    init: Primeiro commit

---
+++ src/main/java/com/dev/willen/eduplanner/EduplannerApplication.java
@@ -0,0 +1,13 @@
+package com.dev.willen.eduplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduplannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduplannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/willen/eduplanner/EduplannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.willen.eduplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduplannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
