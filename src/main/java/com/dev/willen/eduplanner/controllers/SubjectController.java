fe15e166d1219d372b1eda04e2ee603f8c33ac07
willen01 <willen.sesantos@gmail.com>
2025-02-03 10:53:31 -0300

    feat: Implementa remoção de subject cadastrado

--- src/main/java/com/dev/willen/eduplanner/controllers/SubjectController.java
+++ src/main/java/com/dev/willen/eduplanner/controllers/SubjectController.java
@@ -6,10 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/subjects")
@@ -27,4 +24,11 @@ public class SubjectController {
         subjectService.saveSubject(subject, userDetails.getUsername());
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
+
+    @DeleteMapping("/remove/{id}")
+    public ResponseEntity<Void> removeSubject(@PathVariable("id") int id, @AuthenticationPrincipal UserDetails userDetails) {
+        subjectService.removeSubject(id, userDetails.getUsername());
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
 }
--- src/main/java/com/dev/willen/eduplanner/services/SubjectService.java
+++ src/main/java/com/dev/willen/eduplanner/services/SubjectService.java
@@ -4,7 +4,10 @@ import com.dev.willen.eduplanner.dto.CreateSubjectDto;
 import com.dev.willen.eduplanner.entities.Subject;
 import com.dev.willen.eduplanner.entities.User;
 import com.dev.willen.eduplanner.repositories.SubjectRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class SubjectService {
@@ -25,6 +28,19 @@ public class SubjectService {
         subject.setUser(user);
 
         repository.save(subject);
+    }
+
+    public void removeSubject(int subjectId, String userEmail) {
+        User user = userService.getUserByEmail(userEmail);
+        Subject subject = repository.findById(subjectId).orElseThrow(() -> {
+            throw new EntityNotFoundException("Subject with id: " + subjectId + " not found!");
+        });
+
+        if (user.getId() != subject.getUser().getId()) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "User does not have permission to perform this operation");
+        }
+
+        repository.deleteById(subject.getId());
     }
 
 }
